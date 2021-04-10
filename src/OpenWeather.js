cd9ca05f42f132c623c38777f33a5744288dfe36
ABojo <abojo98@gmail.com>
2021-04-10 12:40:14 -0400

    Changed API call to https

--- src/OpenWeather.js
+++ src/OpenWeather.js
@@ -1,6 +1,6 @@
 export default function (apiKey) {
   const getWeatherJSON = async function (location, units) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}&units=${units}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}&units=${units}`;
     const rawResponse = await fetch(url, { mode: 'cors' });
     const jsonData = await rawResponse.json();
 
